cfab769d5bfe8aaf4bdfd7484e9da12ede922070
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-14 13:58:26 +0530

    committing usecase

---
+++ UC8.java
@@ -0,0 +1,22 @@
+package com.LamdaExpression;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC8 {
+	// validating password
+	public void password(String str) {
+		int flag = 0;
+		Pattern pattern = Pattern.compile("^([A-Z]{1,1}[0-9]{1}[_?&*$#@]{1}[a-zA-Z]{6})");
+		Matcher matcher = pattern.matcher(str);
+		while (matcher.find()) {
+			flag = 1;
+		}
+		if (flag == 1) {
+			System.out.println("valid password");
+		} else {
+			System.out.println("invalid password");
+		}
+
+	}
+
+}
