44b787e8e87512d934823bddc9b0cbcbf8c0ae2f
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-14 13:56:46 +0530

    UC5 commit

---
+++ UC5.java
@@ -0,0 +1,21 @@
+package com.LamdaExpression;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC5 {
+	// validating password
+			public void password(String str) {
+				int flag = 0;
+				Pattern pattern = Pattern.compile("^[a-zA-Z0-9-_.?]{8}");
+				Matcher matcher = pattern.matcher(str);
+				while (matcher.find()) {
+					flag = 1;
+				}
+				if (flag == 1) {
+					System.out.println("valid password");
+				} else {
+					System.out.println("invalid password");
+				}
+
+			}
+}
